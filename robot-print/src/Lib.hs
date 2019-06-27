b8ada1ba585de67f1842c00c447b6cc87074dd7f
Lupino <lmjubuntu@gmail.com>
2019-06-27 16:58:30 +0800

    robot-print: Point x y z

--- robot-print/src/Lib.hs
+++ robot-print/src/Lib.hs
@@ -23,13 +23,14 @@ data Robot = Robot
 data Point = Point
   { pointX :: Int
   , pointY :: Int
+  , pointZ :: Int
   } deriving (Show)
 
 zeroPoint :: Point
-zeroPoint = Point 0 200
+zeroPoint = Point 0 200 0
 
 newRobot :: Point -> Robot
-newRobot Point {..} = Robot pointX pointY 0 FaceY []
+newRobot Point {..} = Robot pointX pointY pointZ FaceY []
 
 type Layer = [Point]
 
@@ -121,10 +122,10 @@ moveZ z r | z > robotZ r = moveZ z $ up r
           | otherwise = r
 
 move :: Point -> Robot -> Robot
-move Point {..} = moveY pointY . moveX pointX
+move Point {..} = moveZ pointZ . moveY pointY . moveX pointX
 
 distance :: Point -> Robot -> Int
-distance Point {..} Robot {..} = abs (pointX - robotX) + abs (pointY - robotY)
+distance Point {..} Robot {..} = abs (pointX - robotX) + abs (pointY - robotY) + abs (pointZ - robotZ)
 
 findNearPoint :: [Point] -> Robot -> Maybe (Point, [Point])
 findNearPoint [] _     = Nothing
@@ -142,7 +143,7 @@ printLayer xs r =
 
 printLayers :: [Layer] -> Robot -> Robot
 printLayers []     = id
-printLayers (x:xs) = printLayers xs . up . printLayer x
+printLayers (x:xs) = printLayers xs . printLayer x
 
 ignoreLine :: String -> String
 ignoreLine []         = []
@@ -150,20 +151,20 @@ ignoreLine ('\n': xs) = xs
 ignoreLine (_:xs)     = ignoreLine xs
 
 -- parseLayer xs x y
-parseLayer :: String -> Int -> Int -> Layer -> (Layer, String)
-parseLayer [] _ _ ps             = (ps, [])
-parseLayer ('0':xs) x y ps       = parseLayer xs (x+1) y ps
-parseLayer ('1':xs) x y ps       = parseLayer xs (x+1) y (Point x y : ps)
-parseLayer ('\n':'\n':xs) _ _ ps = (ps, xs)
-parseLayer ('\n':xs) _ y ps      = parseLayer xs (pointX zeroPoint) (y - 1) ps
-parseLayer ('#':xs) x y ps       = parseLayer (ignoreLine xs) x y ps
-parseLayer (_:xs) x y ps         = parseLayer xs x y ps
+parseLayer :: String -> Int -> Int -> Int -> Layer -> (Layer, String)
+parseLayer [] _ _ _ ps             = (ps, [])
+parseLayer ('0':xs) x y z ps       = parseLayer xs (x+1) y z ps
+parseLayer ('1':xs) x y z ps       = parseLayer xs (x+1) y z (Point x y z : ps)
+parseLayer ('\n':'\n':xs) _ _ _ ps = (ps, xs)
+parseLayer ('\n':xs) _ y z ps      = parseLayer xs (pointX zeroPoint) (y - 1) z ps
+parseLayer ('#':xs) x y z ps       = parseLayer (ignoreLine xs) x y z ps
+parseLayer (_:xs) x y z ps         = parseLayer xs x y z ps
 
-parseLayers :: String -> [Layer]
-parseLayers [] = []
-parseLayers xs =
-  case (parseLayer xs (pointX zeroPoint) (pointY zeroPoint) []) of
-    (layer, ps) -> layer : parseLayers ps
+parseLayers :: Int -> String -> [Layer]
+parseLayers _ [] = []
+parseLayers z xs =
+  case (parseLayer xs (pointX zeroPoint) (pointY zeroPoint) z []) of
+    (layer, ps) -> layer : parseLayers (z + 1) ps
 
 getMinY :: [Layer] -> Int
 getMinY [] = pointY zeroPoint
@@ -193,14 +194,13 @@ someFunc =  do
   let file = case args of
                []    -> defaulFile
                (x:_) -> x
-  layers <- parseLayers <$> readFile file
+  layers <- parseLayers 0 <$> readFile file
   let initPoint = zeroPoint { pointY = (getMinY layers) }
   putStrLn
     $ flip printActions 0
     . robotActions
     . turnFace FaceY
-    . move initPoint
-    . moveZ 0
+    . moveX (pointX initPoint)
     . move initPoint {pointX = pointX initPoint - 1}
     . printLayers layers
     $ newRobot initPoint
