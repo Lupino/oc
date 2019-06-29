d64390734f3af9fb4a24e729bb7553a5b52aa205
Lupino <lmjubuntu@gmail.com>
2019-06-29 22:20:21 +0800

    fixed. wrong get src2

--- craft/craft_block1.lua
+++ craft/craft_block1.lua
@@ -4,7 +4,7 @@ function main()
     local running = true
     local src0 = getItemName(1)
     local src1 = getItemName(2)
-    local src2 = getItemName(2)
+    local src2 = getItemName(3)
     while running do
         mergeItems()
         if not craft.crafting1(src0) then
