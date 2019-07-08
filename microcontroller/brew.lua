d0ff6cfb0c221b621268e8a74857cf728a925599
Lupino <lmjubuntu@gmail.com>
2019-07-08 09:06:56 +0800

    add brew.lua

---
+++ microcontroller/brew.lua
@@ -0,0 +1,45 @@
+local r = component.proxy(component.list("redstone")())
+
+local brew_tick = 1
+local collect_tick = 0
+
+local brew_state = true
+local collect_state = false
+
+local brew_delay = 24
+local collect_delay = 22
+
+local collect_pin = 5
+
+r.setOutput(brew_pin, 15)
+r.setOutput(collect_pin, 0)
+
+while true do
+    computer.pullSignal(1)
+    if brew_tick > brew_delay then
+        if brew_state then
+            r.setOutput(brew_pin, 0)
+            brew_state = false
+            brew_delay = 6
+        else
+            r.setOutput(brew_pin, 15)
+            brew_state = true
+            brew_delay = 24
+        end
+        brew_tick = 0
+    end
+    if collect_tick > collect_delay then
+        if collect_state then
+            r.setOutput(collect_pin, 0)
+            collect_state = false
+            collect_delay = 22
+        else
+            r.setOutput(collect_pin, 15)
+            collect_state = true
+            collect_delay = 8
+        end
+        collect_tick = 0
+    end
+    brew_tick = brew_tick + 1
+    collect_tick = collect_tick + 1
+end
