085f37ae28cb9f70281184c98356572c2048248e
Lupino <lmjubuntu@gmail.com>
2019-07-29 15:41:33 +0800

    use urlencode

--- microcontroller/control.lua
+++ microcontroller/control.lua
@@ -6,9 +6,20 @@ local token = '9e899f11-1560-458b-8492-2723a2374d8a'
 local url = 'http://superapp.huabot.com/api/devices/' .. uuid .. '/rpc/'
 local id = '1'
 
+-- This makes a string safe for being used in a URL.
+function encode(code)
+  if code then
+    code = string.gsub(code, "([^%w ])", function (c)
+      return string.format("%%%02X", string.byte(c))
+    end)
+    code = string.gsub(code, " ", "+")
+  end
+  return code
+end
+
 function relaySwitch(onoff)
-    local command = '%7B%22method%22%3A%22relay_' .. onoff .. '%22%2C%22index%22%3A' .. id .. '%7D'
-    local data = 'data=' .. command .. '&token=' .. token .. '&format=json'
+    local command = '{"method": "relay_' .. onoff .. '", "index": ' .. id .. '}'
+    local data = 'data=' .. encode(command).. '&token=' .. token .. '&format=json'
     local ret, res = pcall(internet.request, url, data)
     return ret
 end
