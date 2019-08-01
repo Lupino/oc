9680682e98a74e5bfd0de5eff17cbc6e70c97a5f
Lupino <lmjubuntu@gmail.com>
2019-08-01 12:03:16 +0800

    update count

--- robot-print/scripts/count.py
+++ robot-print/scripts/count.py
@@ -1,5 +1,10 @@
 import sys
+import math
 
 with open(sys.argv[1], 'r') as f:
     data = f.read()
-    print(data.count('P'))
+    count = data.count('P')
+
+    print('Total: {}'.format(count))
+    print('Need: {} * 64'.format(math.ceil(count / 64)))
+    print('IU: {} * 64 * 16'.format(math.ceil(count / 64 / 16)))
